c207cbbdc9adfb4714952082f2470178a1b287a9
max-ajwang <maxajwang@gmail.com>
2025-02-03 16:20:43 +0300

    Server Update

--- server.js
+++ server.js
@@ -16,39 +16,39 @@ import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
 
 // //public
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-// import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // //middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authMiddleware.js';
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.CLOUD_API_KEY,
-//   api_secret: process.env.CLOUD_API_SECRET,
-// });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
-// app.use(helmet());
-// app.use(mongoSanitize());
+app.use(helmet());
+app.use(mongoSanitize());
 
 app.use('/api/v1/paybills', authenticateUser, paybillRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
